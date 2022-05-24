b550164c988877f24b9f6e93af84f6e701648992
Rafael Silva dos Anjos <rafael.sanjos@gmail.com>
2022-05-24 12:09:04 -0300

    Add banco de dados, schema da tabela tarefas

--- main.py
+++ main.py
@@ -1,5 +1,30 @@
+from typing import List
+import databases
+import sqlalchemy
+
 from fastapi import FastAPI
 from pydantic import BaseModel
+
+DATABASE_URL = "sqlite:///./FastRicardoDB.db"
+
+database = databases.Database(DATABASE_URL)
+
+metadata = sqlalchemy.MetaData()
+
+# Criando um squema para tabela tarefas
+tarefas = sqlalchemy.Table(
+    "tarefas",
+    metadata,
+    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+    sqlalchemy.Column("nome", sqlalchemy.String),
+    sqlalchemy.Column("concluido", sqlalchemy.Boolean),
+)
+
+engine = sqlalchemy.create_engine(
+    DATABASE_URL, connect_args={"check_same_thread": False}
+)
+metadata.create_all(engine)
+
 
 app = FastAPI()
 
