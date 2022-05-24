d7f267b4dfeb28ec1874588c078167fba19ad884
Rafael Silva dos Anjos <rafael.sanjos@gmail.com>
2022-05-24 16:13:59 -0300

    listagem de tarefas e insert em banco de dados

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from email.policy import default
+from pickle import FALSE, TRUE
 from typing import List
 import databases
 import sqlalchemy
@@ -16,7 +18,7 @@ tarefas = sqlalchemy.Table(
     "tarefas",
     metadata,
     sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
-    sqlalchemy.Column("nome", sqlalchemy.String),
+    sqlalchemy.Column("descricao", sqlalchemy.String),
     sqlalchemy.Column("concluido", sqlalchemy.Boolean),
 )
 
@@ -28,14 +30,24 @@ metadata.create_all(engine)
 
 app = FastAPI()
 
-
+# Classe modelo de negocio
 class Tarefa(BaseModel):
     id: int
-    name: str
+    descricao: str
     concluido: bool
 
+class TarefaCadastro(BaseModel):
+    descricao: str
 
-lista_tarefas_db = []
+
+@app.on_event("startup")
+async def startup():
+    await database.connect()
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
 
 
 @app.get("/")
@@ -43,12 +55,39 @@ async def home_or_index():
     return "Pagina inicial"
 
 
-@app.get("/api/tarefas")
+@app.get("/api/tarefas", response_model=List[Tarefa])
 async def get_terefas():
-    return lista_tarefas_db
+    query = tarefas.select()
+    return await database.fetch_all(query)
 
 
-@app.post("/api/tarefas")
-async def create_tarefa(tarefa: Tarefa):
-    lista_tarefas_db.append(tarefa)
-    return tarefa
+@app.post("/api/tarefas", response_model=Tarefa)
+async def create_tarefa(tarefa: TarefaCadastro):
+    query = tarefas.insert().values(descricao=tarefa.descricao, concluido=False)
+    last_record_id = await database.execute(query)
+    return Tarefa(id=last_record_id, descricao=tarefa.descricao, concluido=False)
+
+@app.delete("/api/tarefas/{id}")
+async def delete_tarefa(id: int):
+    # apagar uma tarefa
+
+
+@app.get("/api/tarefas/{id}", response_model=Tarefa)
+async def get_terefa():
+    # obj 01: consulta a tarefa pelo id e retorna ela.
+
+    # obj 02: se não existe retorne 404
+
+#deixe por ultimo
+@app.put("/api/tarefas/concluido/{id}", response_model=Tarefa)
+async def update_tarefa(id: int):
+    # codigo inicio
+    # buscar no banco pelo ID (cenario registro que existe) (2 linhas de codigo)
+    # tarefa.concluido = not tarefa.concluido
+    # atualiza o banco de dados com o novo valor do atributo concluido (update) (2 linhas de codigo)
+    # retornar objeto tarefa alterado
+    return Tarefa(id=id, descricao=f"Você deve trocar o valor de concluido. da tarefa {id}", concluido=False)
+    #codigo termino
+
+    #depois - depois que o update tiver funcionando
+    #se registro da tarefa não existe retorne status code 404 - Not Found
